b4b0c09f4dcbdddb0c372057a747afaef3e6f936
Ecos1213 <diasflac1213@gmail.com>
2025-02-26 12:17:55 -0500

    Agrega clase de declarar funciones sin tener problema con palabras reservadas y algunos conocimientos de estructura de kotlin

---
+++ src/Fundamentos.kt
@@ -0,0 +1,3 @@
+fun main() { // para poder ejecutar codigo principal puro necesitamos una funcion principal, por eso declaramos la funcion main con la palabra reservada fun
+    print("Daniel") // print se puede invocar sin necesidad de librerias ya que esta en el estandard de la libreria de kotlin
+}
