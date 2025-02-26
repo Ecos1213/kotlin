6ca3142d7ae0347b6adec9bd2a88b14242a99de2
Ecos1213 <diasflac1213@gmail.com>
2025-02-26 10:39:42 -0500

    Agrega clase de aprender a comentar de un bloque como multiple linea y configuracion como de firma y de keymaps

---
+++ src/Comentarios.kt
@@ -0,0 +1,11 @@
+// Archivo para aprender a comentar
+// Subtitulo de nuestro archivo
+/*
+* Comentarios en bloque
+* para añadir
+* multiples lineas
+* */
+
+// en el menu del ide settings y keymap buscamos comment y vemos que para comentar usamos o ctrl + / para una linea y si es multiple ctrl + sifht + /
+// en el mismo menu podemos entrar a settings despues a editor y en code and template ahi veremos una pestaña que dice include y en file header podemos colocar la forma, como vemos al lado de file header hay una taza por que es java no te preocupes por que si buscamos la de kotlin que esta en la pestaña file buscamos kotlin file y ahi veremos que agrega cualquier comentario de java
+
