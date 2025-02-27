5ae2c1230c4b7856ddd674001f3e617da003c125
Ecos1213 <diasflac1213@gmail.com>
2025-02-27 15:51:41 -0500

    Agrega clase de diferentes forma de declaracion de variables

--- holamundo.go
+++ holamundo.go
@@ -1,38 +1,39 @@
-/*
-En golang podemos usar el comando
-go version #para mirar que version tenemos de golang
-go env # para ver todas las caracteristicas y path que tiene golang
-esto es importante porque sucede que si queremos trabajar con proyecto tenemos que dejarlo en el GOPATH
-y si es la primera vez que instalamos golang tenemos que crear la carpeta go y dentro de go creamos la carpeta src
-esta carpeta de go es importante porque aca estara todos los bins inclusos librerias externas como las librerias de golang
-tambien si queremos trabajar solo con golang sin github tendriamos que crear esta carpeta aunque tambien podriamos trabajar con github
+package main
 
-todas las extensiones de go es .go
-*/
-package main // con package le decimos a que paquete pertenece este archivo de go y en este caso tiene main y le estamos diciendo que este archivo pertenece al paquete main, este paquete main hace referencia que este archivo es el archivo principal, este archivo principal es el primero en ejecutarse
-//import "fmt" // asi importamos una libreria interna de golang
-import ( // asi importamos multiples librerias internas como externas de golnag
+import (
 	"fmt"
 
-	"rsc.io/quote" // como vemos para importar una libreria externa simplemente colocamos toda nuestra ruta externas despues de usar go get
+	"rsc.io/quote"
 )
 
-func main() { // esta funcion main es la funcion principal es la funcion que se ejecuta primero
-	fmt.Println("Hola Mundo")  // para imprimir en consola con salto de linea usamos la funcion Println y esta viene del paquete fmt
-	fmt.Println(quote.Hello()) // ahora podemos invocar metodos de nuestra libreria externa
+var estaFuera, estaDentro = true, false // se puede inicializar variables incluso afuerda de las funciones
+
+func main() {
+	fmt.Println("Hola Mundo")
+	fmt.Println(quote.Hello())
+	//Declaracion de variables
+	var firstName string      // almacena cadena de caracteres, usamos la palabra reservada var para declarar una variable, las variables tienen que estar declaradas si no go no nos compilara incluso nos dara un error, ademas de esta forma se tiene que colocar el tipo de dato
+	var edad, apellido string // en golang se puede hacer asignacion multiple como python, pero solo funciona si las variables van hacer del mismo tipo
+	var age int
+
+	var ( // asi tambien podemos declarar varias variables de diferente tipo, se puede asignar valores incluso dentro y despues pero recuerda que si no se asigna un valor dara un error
+		dia, año string = "1", "2020" // incluso podemos declarar varias variables en una misma linea pero tiene que ser del mismo tipo
+		mes      int    = 1
+		esMayor         = true // tambien lo que podemos hacer es declarar la variable y a la vez asignar un valor sin su tipo de dato para que golang lo interprete pero esto solo lo podemos hacer si asignamos un valor si no nos dara un error
+	)
+
+	var identidad, telefono, correo, diaInicial = "123456", "3169035844", "diasflac1213@gmail.com", 22 // tambien podemos simplifcar mas la inicializacion y declaracion de variables como python
+
+	firstName = "Alex"
+	apellido = "Rodel"
+	edad = "27"
+	age = 27
+	// en golang si no usamos una variable nos dara un error
+	fmt.Println(identidad, estaFuera, estaDentro, telefono, correo, diaInicial, firstName, apellido, age, edad, dia, año, mes, esMayor) // asi podemos imprimir varias variables y es como python que deja un espacio
+
+	// otra manera de inicializar las variables es de esta forma
+	esPaco, cedula, celular := false, "1143829274", 11 //esta es la forma mas comun de declarar pero con la palabra var puedes declararla fuera de las funciones, mientras que el simbolo := que hace referencia a golang solo puede declarar variables de esta forma dentro de funciones
+	celular = 3168035844
+	fmt.Println(esPaco, cedula, celular)
+
 }
-
-// con go run nombrearchivo.go ejecutamos el archivo en la terminal
-// con go build nombrearchivo.go construimos un bin o .exe para producion
-
-// para poder usar paquetes de terceros necesitamos un inicializador de paquetes,
-// con go mod init nombreconquevamosamanejarlosmodulos #inicializamos el manejador de paquetes
-// esto creara un archivo llamado go.mod este contiene module nombreconquevamosamanejarlosmodulos este es el nombre del manejador de modulo y aparece la version de golang
-// cuando empezemos a trabajar con los archivos externos tambien aparecera un archivo que es go.sum, este es un archivo que se utiliza para la suma de verificacion de los modulos y dependencias de nuestro proyecto
-
-//si queremos descargar un paquete usamos el siguiente comando
-// go get rutadelrepositoriodelpaquete
-//este repositorio puede estar en github o rsc el go get esto descargara el repositorio y se encargara de configurarlo todo, incluso lo agregara automaticamente al manejador de modulos osea go.mod
-
-// instalamos go get rsc.io/quote quote lo que hace es un paquete con las frases mas famosas
-// si vemos el go.mod veremos que dice require() y dentro de require veremos que estan los paquetes que agrego go get
